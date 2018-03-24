ff5fe6401c2da7c82b1bf6d2f7b8866e82875641
DomGrieco <dominic837@gmail.com>
2018-03-24 16:28:54 -0300

    Using settings.txt for paths
    Currently settings.txt needs to be in base directory of chaos.exe and ChaosLauncher.exe

--- Chaos/Server/Paths.cs
+++ Chaos/Server/Paths.cs
@@ -10,23 +10,31 @@
 // ****************************************************************************
 
 using System;
+using System.CodeDom;
 using System.Configuration;
+using System.IO;
+using System.Text;
 
 namespace Chaos
 {
     public static class Paths
     {
+        //Get Current Directoy 
+        private static readonly string SolutionDir = Directory.GetCurrentDirectory();
+        //Path of settings.txt
+        public static string Path = $@"{SolutionDir}\settings.txt";
+        //Read settings.txt
+        public static string[] ReadSettings = File.ReadAllLines(Path, Encoding.UTF8);
         //primary directory, change to your own~
-        public static string BaseDir = ConfigurationManager.AppSettings[@"BaseDir"];
+        public static string BaseDir = ReadSettings[0];
         //dark ages directory
-        public static string DarkAgesDir = ConfigurationManager.AppSettings[@"DarkAgesDir"];
+        public static string DarkAgesDir = ReadSettings[1];
         //dark ages executable
         public static string DarkAgesExe => $@"{DarkAgesDir}Darkages.exe";
         //dynamic host name, change to your own~
-        public static string HostName = ConfigurationManager.AppSettings[@"HostName"];
+        public static string HostName = ReadSettings[2];
         //redis config string (host, port)
-        public static string RedisConfig = ConfigurationManager.AppSettings[@"RedisConfig"];
-
+        public static string RedisConfig = ReadSettings[3];
 
         public static string LogFiles => $@"{BaseDir}logs\";
         public static string MetaFiles => $@"{BaseDir}metafiles\";
